a09562bfdde16b1fce0bffab210f43bde3073850
Alexis <alexisguilbault@outlook.com>
2025-09-05 15:29:52 -0400

    Adaptation contrôles

--- guilbault_alexis_tp1/scenes/objets/volume.gd
+++ guilbault_alexis_tp1/scenes/objets/volume.gd
@@ -1,5 +1,4 @@
 extends AnimatedSprite2D
-
 var audio_joue = true
 var temps
 
@@ -7,15 +6,14 @@ func _ready() -> void:
 	$"../AudioStreamPlayer".play()
 
 func _input(event):
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_M:
-			if audio_joue == true:
-				temps = $"../AudioStreamPlayer".get_playback_position()
-				$"../AudioStreamPlayer".stop()
-				audio_joue = false
-				frame = 1
-			else:
-				$"../AudioStreamPlayer".play()
-				$"../AudioStreamPlayer".seek(temps)
-				audio_joue = true
-				frame = 0
+	if Input.is_action_just_pressed("Mute"):
+		if audio_joue == true:
+			temps = $"../AudioStreamPlayer".get_playback_position()
+			$"../AudioStreamPlayer".stop()
+			audio_joue = false
+			frame = 1
+		else:
+			$"../AudioStreamPlayer".play()
+			$"../AudioStreamPlayer".seek(temps)
+			audio_joue = true
+			frame = 0
